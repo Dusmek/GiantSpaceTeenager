b813bf0ce7e91fea08508f9dc24efc6f182d6309
kuba <jakubstanislaworlinski@gmail.com>
2019-04-16 17:47:57 +0200

    new planet system

--- Node2D.gd
+++ Node2D.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
-var minx = 100
-var maxx = 700
-var miny = 100
-var maxy = 700
+#var minx = 100
+#var maxx = 700
+#var miny = 100
+#var maxy = 700
+
+var minx = 0
+var maxx = 1000
+var miny = 0
+var maxy = 1000
 
 var maxinstupdateperframe = 25*500;
 var currpostponeind = 0
@@ -14,6 +19,8 @@ class obj :
 	var position=Vector2(0,0)
 	var speed = Vector2(0,0)
 	var force = Vector2(0,0)
+	var angle = 0
+	var angularspeed = 0
 	var mass = 0.2
 	var radius = 1
 	var currind=0
@@ -32,23 +39,34 @@ class obj :
 			inst.force-=inst.speed*inst.speed.length()/20
 			inst.myupdate(delta)
 			inst.force=Vector2(0,0)
-		var koniec = min(instances.size(),currind+60)
-		for inst1 in range(currind,koniec):
-			instances[inst1].force+=(position-instances[inst1].position)*4
+		var koniec = min(instances.size(),currind+40)
+		for inst1id in range(currind,koniec):
+			var inst1 = instances[inst1id]
+			
+			inst1.force+=(position-inst1.position)*(1+0.8*(3-inst1.type)*(3-inst1.type))
+			var any = false
 			for inst2 in instances:
-				if instances[inst1]==inst2:
+				if inst1==inst2:
 					continue
-				var d = instances[inst1].position-inst2.position
+				var d = inst1.position-inst2.position
 				var lens = d.length()
 				if lens>biggestdist:
 					biggestdist=lens
 				if lens<6:
-					var n = d.normalized()
-					instances[inst1].force+=n*(12-lens)*4
-					inst2.force-=n*(12-lens)*4
+					any=true
+					var n = d.normalized()*(12-lens)
+					if inst1.type == inst2.type and inst2.type==1:
+						var stren = 3
+						inst1.force+=n*stren
+						inst2.force-=n*stren
+					else:
+						inst1.force+=n*4
+						inst2.force-=n*4
+					inst2.speed*=0.95
+					
 		currind = koniec
 		if currind>=instances.size():
-			radius=biggestdist*0.45
+			radius=radius *(1-delta) + delta * biggestdist*0.45
 			biggestdist=0
 			if instances.size()==1:
 				radius=0.3
@@ -58,10 +76,33 @@ class obj :
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var el = load("res://element.tscn")
-		
-	for i in range(1000):
+	var maxr = (maxx-minx)*0.5
+	var Voronoi = []
+	var Vtypes = []
+	randomize()
+	for i in range(10):
+		var temp = floor(rand_range(0,1)*4)
+		print(temp)
+		Vtypes.append(temp);
+		Voronoi.append(Vector2(rand_range(minx,maxx),rand_range(miny,maxy)))
+	var sr = Vector2(maxx+minx,maxy+miny)*0.5
+	for i in range(2000):
 		var inst = el.instance()
-		inst.position = Vector2(rand_range(minx+200,maxx-200),rand_range(miny+200,maxy-200))
+		var r = rand_range(1,maxr)
+		r= (maxr-r)
+		r*=r/maxr
+		r = maxr-r
+		var a = rand_range(0,2*PI)
+		inst.position = Vector2(0,1).rotated(a) * r + sr
+		var t = 0
+		var closd = 10000
+		for vi in range(Voronoi.size()):
+			var cd = (Voronoi[vi]-inst.position).length()
+			if cd<closd:
+				t = Vtypes[vi]
+				print(t)
+				closd = cd
+		inst.init(t) 
 		var o = obj.new()
 		o.init(inst)
 		add_child(inst)
@@ -72,7 +113,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	print(1/max(0.01,delta))
-	var toincrease = []
 	var toremove = []
 	var count = allinst.size()
 	
@@ -95,6 +135,7 @@ func _process(delta):
 				i1.mass+=i2.mass
 				for p in i2.instances:  
 					i1.instances.append(p)
+					p.speed = i2.speed
 				i2.instances.clear()
 			else:
 				var f = (p1-p2).normalized()*250/max(25,(l*l))
@@ -109,22 +150,25 @@ func _process(delta):
 	for i in range(allinst.size()):
 		allinst[i].update(delta)
 		
-		if(Input.is_mouse_button_pressed(BUTTON_LEFT)):
+		if(Input.is_mouse_button_pressed(BUTTON_LEFT) or Input.is_mouse_button_pressed(BUTTON_RIGHT)):
 			var p = get_viewport().get_mouse_position() - allinst[i].position
 			var l = p.length()
-			allinst[i].force = p.normalized()*250000/(max(250,p.length()*p.length()))
+			if Input.is_mouse_button_pressed(BUTTON_RIGHT):
+				var s=0
+				if l<200:
+					s=1
+				allinst[i].force = -allinst[i].speed*s#(max(0,(200-l)/200))
+			else:
+				allinst[i].force = p*2500000/(max(25000,p.length_squared()*p.length_squared()))
 		else:
 			allinst[i].force=Vector2(0,0)
 			
 		var sr = Vector2(maxx+minx, maxy+miny)/2
 		var lsa = allinst[i].position -sr
 		
-		var ls2 = lsa.length_squared()
-		if ls2 > (maxx-minx)*(maxx-minx)/4:
-			allinst[i].position = sr+lsa.normalized()*(maxx-minx)/2
-			allinst[i].speed *= -0.7
-		var lsn = (allinst[i].position -sr).normalized()
-		allinst[i].force += Vector2(lsn.y,-lsn.x)*0.001*ls2/max(1,allinst[i].speed.length_squared())
+		var ls2 = lsa.length()-allinst[i].radius
+		if ls2 > (maxx-minx)/2:
+			allinst[i].speed *= -0.5
 		
 	print("start: " + str(currpostponeind) + " " + str(koniec))
 	currpostponeind=koniec+1
--- element.gd
+++ element.gd
@@ -11,22 +11,24 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+var type = 0
 var speed = Vector2(0,0)
 var force = Vector2(0,0)
 func myupdate(delta):
 	speed+=force*delta
 	position+=speed*delta
 	pass
-	
+func init(t):
+	type=t
+	pass
 func _on_Sprite_ready():
 	var atom_sprite = get_node("Sprite_node")
-	var r = randi()%4
-	if r==0:
+	if type==0:
 		atom_sprite.set_texture(load("atom1.png"))
-	elif r==1:
+	elif type==1:
 		atom_sprite.set_texture(load("atom2.png"))
-	elif r==2:
+	elif type==2:
 		atom_sprite.set_texture(load("atom3.png"))
-	elif r==3:
+	elif type==3:
 		atom_sprite.set_texture(load("atom4.png"))
 	pass # Replace with function body.
